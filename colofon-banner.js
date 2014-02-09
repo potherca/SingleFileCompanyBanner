6d4e235b99d0f2ca7ac79cc2afbd557d313272c6
Ben Peachey <potherca@gmail.com>
2014-02-09 14:54:19 +0100

    Makes logo, link color and hover color configurable, instead of requiring a Company style to be defined in the CSS.

--- colofon-banner.js
+++ colofon-banner.js
@@ -5,11 +5,11 @@
             for(var i = 0, iLength = this.length; i < iLength; ++i) {
                 p_fCallback.call(p_sScope, this[i], i, this);
             }
-        }
+        };
     }
 
     function getQueryParameters(p_sUrl) {
-        var oParameters, sQuery, aKeyValuePairs;
+        var oParameters, sQuery, aKeyValuePairs, aKeyValuePair;
 
         oParameters = {};
         sQuery = p_sUrl.split('?')[1] || '';
@@ -23,8 +23,12 @@
         return oParameters;
     }
 
-    function addBanner(p_oOptions) {
-        var sCss, oStyleNode, oFooterNode;
+    function cssSafeCompanyName(p_sCompanyName){
+        return p_sCompanyName.replace(/[^a-zA-z0-9\-_]/g, '');
+    }
+
+    function getCss(p_oOptions){
+        var sCss;
 
         sCss = function(){/*
             @import url(http://fonts.googleapis.com/css?family=Roboto+Slab:400,300|Roboto:400,300|Playball);
@@ -40,6 +44,7 @@
                 padding: 1em 8em 1em 1em;
                 position: relative;
                 text-align: center;
+                z-index: 9999;
             }
 
             .colofon a {
@@ -49,31 +54,46 @@
                 border: none;
                 background-image: none;
             }
+
             .colofon a:after {}
 
-            .colofon-beanmachine a {
-                color: rgb(0, 175, 219);
+            .colofon-{{sCompanyName}} a {
+                color: {{sLinkColor}};
                 font-family: "Roboto Slab",Arial;
             }
 
-            .colofon-beanmachine:hover {
-                box-shadow: inset 0 0 0.75em rgb(0,0,0), inset 0 0 2em rgba(0, 175, 219, 0.65);
+            .colofon-{{sCompanyName}}:hover {
+                box-shadow: inset 0 0 0.75em rgb(0,0,0), inset 0 0 2em {{sHoverColor}};
             }
 
-            .colofon-beanmachine .company-name {
-                background-image: url(http://thebeanmachine.accept.starsale.nl/Static/Images/UserUpload/over/logo.png);
+            .colofon-{{sCompanyName}} .company-name {
+                background-image: url({{sLogo}});
                 background-size: contain;
                 background-position: center right;
                 background-repeat: no-repeat;
                 color: rgba(0,0,0,0) !important;
                 display: table-cell;
                 height: 6em;
+                width: 6em;
                 position: absolute;
-                right: 0;
+                right: 0.85em;
                 text-shadow: none;
-                top: 1.25em;
+                top: 0;
             }
         */}.toString().slice(14,-3);
+
+       sCss = sCss.replace(/{{sLinkColor}}/g, p_oOptions.linkColor);
+       sCss = sCss.replace(/{{sHoverColor}}/g, p_oOptions.hoverColor);
+       sCss = sCss.replace('{{sLogo}}', p_oOptions.logo);
+       sCss = sCss.replace(/{{sCompanyName}}/g, cssSafeCompanyName(p_oOptions.companyName));
+
+       return sCss;
+    }
+
+    function addBanner(p_oOptions) {
+        var sCss, oStyleNode, oFooterNode;
+
+        sCss = getCss(p_oOptions);
 
         oStyleNode = document.createElement('style');
         oStyleNode.type = 'text/css';
@@ -83,33 +103,37 @@
             oStyleNode.appendChild(document.createTextNode(sCss));
         }
         (document.getElementsByTagName('head')[0]||document.getElementsByTagName('html')[0]).appendChild(oStyleNode);
-        
+
         oFooterNode = document.createElement('footer');
-        oFooterNode.className = 'colofon ' + p_oOptions.cssClass;
-        oFooterNode.innerHTML = '<a href="' + p_oOptions.url +'" target="_blank">This experiment brough to you thanks to my employer <span class="company-name">' + p_oOptions.companyName + '</span></a>';    
+        oFooterNode.className = 'colofon colofon-' + cssSafeCompanyName(p_oOptions.companyName);
+        oFooterNode.innerHTML = '<a href="' + p_oOptions.url +'" target="_blank">This experiment brough to you thanks to my employer <span class="company-name">' + p_oOptions.companyName + '</span></a>';
         (document.getElementsByTagName('body')[0]||document.getElementsByTagName('html')[0]).appendChild(oFooterNode);
     }
-    
+
     function getParameters(){
         var oColofonParameters, oParameters, aScripts, iLastScript, sIndex;
-        
+
         oColofonParameters = {
               url : ''
-            , cssClass : ''
             , companyName : ''
+            , logo : ''
+            , linkColor : ''
+            , hoverColor : ''
         };
         aScripts = document.getElementsByTagName('script');
         iLastScript = aScripts.length - 1;
-        oParameters = getQueryParameters(aScripts[iLastScript].src)
-        
+        oParameters = getQueryParameters(aScripts[iLastScript].src);
+
         for(sIndex in oParameters){
+            //noinspection JSUnfilteredForInLoop
             if(typeof oColofonParameters[sIndex] !== 'undefined'){
+                //noinspection JSUnfilteredForInLoop
                 oColofonParameters[sIndex] = decodeURIComponent(oParameters[sIndex]);
             }
         }
-        
+
         return oColofonParameters;
     }
-    
+
     addBanner(getParameters());
 }());
